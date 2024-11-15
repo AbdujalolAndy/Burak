470a2f8329f31d6f786aac8c1e42bf27b4142fc6
AbdujalolAndy <mrarnoldunlimited@gmail.com>
2024-11-16 04:00:12 +0900

    feat: Integration of session authontication

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,16 @@ import morgan from "morgan";
 import routerAdmin from "./routers/router-bssr"
 import routerMain from "./routers/router-spa";
 
+import session from "express-session";
+import MongodbSession from "connect-mongodb-session";
+
+
 const app = express();
+const MongodbStore = MongodbSession(session);
+const store = new MongodbStore({
+    uri: String(process.env.MONGODB_URL),
+    collection: "sessions"
+})
 
 /** 1-ENTERANCE **/
 app.use(express.static(path.join(__dirname, "public")));
@@ -13,6 +22,17 @@ app.use(express.json());
 app.use(morgan(`:method :url :response-time ms [:status] \n`))
 
 /** 2-SESSION **/
+app.use(
+    session({
+        secret: String(process.env.SECRET_TOKEN),
+        cookie: {
+            maxAge: 1000 * 3600 * 6 // 6h
+        },
+        store,
+        resave: true,
+        saveUninitialized: true,
+    })
+)
 
 /** 3-VIEWS **/
 app.set("views", path.join(__dirname, "views"));
